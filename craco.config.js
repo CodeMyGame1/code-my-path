873f32a729a47ae85a31fb7f9e820558b7de8e45
CodeMyGame1 <40456953+CodeMyGame1@users.noreply.github.com>
2025-04-16 15:20:39 -0700

    Chore: setup Heroku hosting (#1)
    * chore: use port specified in configuration environment
    
    * chore: make default tab size 2
    
    * chore: add Procfile

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,21 @@
-// The reason we need to use CRACO is because MDX doesn't work with Create React App out of the box.
+// The reason we need to use CRACO is because MDX doesn't work with Create React
+// App out of the box.
 // https://github.com/orgs/mdx-js/discussions/1870
 // https://github.com/orgs/mdx-js/discussions/2218
 
 const { addAfterLoader, loaderByName } = require("@craco/craco");
+const dotenv = require("dotenv");
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+dotenv.config();
 
 module.exports = async env => {
   const remarkGfm = (await import("remark-gfm")).default;
 
   return {
+    devServer: {
+      port: process.env.PORT
+    },
     webpack: {
       configure: webpackConfig => {
         addAfterLoader(webpackConfig, loaderByName("babel-loader"), {
